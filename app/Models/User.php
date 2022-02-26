03fdadd00271788284ce97c3daae911697e89e0d
Turhan <turhan.cetin@detaysoft.com>
2022-02-26 14:30:59 +0300

    Auth değilken yanlış logine yönlendirme düzenlendi

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -17,6 +16,7 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+    protected $table = 'users';
     protected $fillable = [
         'name',
         'email',
@@ -41,4 +41,5 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
 }
--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 Route::controller(UserController::class)->group(function () {
     Route::post('user/register', 'store');
-    Route::post('/login', 'login');
+    Route::post('/login', 'login')->name('login');
     Route::group(['middleware' => ['auth:sanctum']], function (){
         Route::resource('user', UserController::class);
         Route::post('user/logout','logout');
