0e4ae663ac841c5a00685f67e67c96f45f076638
Turhan <turhan.cetin@detaysoft.com>
2022-03-01 14:42:13 +0300

    File Structer ve docker-compose.yml ve Dockerfile dosyaları düzenlendi

---
+++ backend/routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+Route::controller(UserController::class)->group(function () {
+    Route::post('user/register', 'store');
+    Route::post('/login', 'login')->name('login');
+    Route::group(['middleware' => ['auth:sanctum']], function (){
+        Route::resource('user', UserController::class);
+        Route::post('user/logout','logout');
+    });
+});
+
