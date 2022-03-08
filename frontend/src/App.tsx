6896ba4daa4ae19979e0d8fd22f2edd806372da1
Turhan <turhan.cetin@detaysoft.com>
2022-03-08 13:48:49 +0300

    Typescript not module ve eslint hatası giderildi.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Layout from "./Components/Layout/Layout";
-import Header from "./Components/Header/Header";
 
 function App() {
   return (
-        <Layout>
-            <Header/>
-            <div>Hello</div>
-        </Layout>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
